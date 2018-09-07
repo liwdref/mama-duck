5e2ed141df006f09ff76ae1d683a99cf8baf03f8
Wilfred Ian Benite <liwdref13@gmail.com>
2018-09-07 11:30:13 +0800

    added more JS

---
+++ js/game-assets/baby_duck.js
@@ -0,0 +1,12 @@
+/*
+var baby_duck;
+
+
+function get_b_duck(){
+    baby_duck = game.add.group();
+
+    baby_duck.x= 400;
+    baby_duck.y=300;
+    
+} 
+*/
--- js/game.js
+++ js/game.js
@@ -1,31 +1,80 @@
 import Phaser from "phaser";
 
-import mama_duck from "../assets/mama_duck.png";
-import mama_grab from "../assets/mama_grab.png";
-import water from "../assets/water.png";
+import mama_duck_pic from "../assets/sprites/mama_duck.png";
+import mama_grab_pic from "../assets/sprites/mama_grab.png";
+
+import baby_duck_pic from "../assets/sprites/baby_duck.png";
+
+import water_pic from "../assets/water.png";
+import cursor_pic from "../assets/cursor.png";
+
+
+var mama;
+var baby;
+var curs;
 
 function preload() {
-  this.load.image("mama_duck", mama_duck);
-  this.load.image("mama_grab", mama_grab);
-  this.load.image("water", water);
+
+  this.load.image("mama_duck_pic", mama_duck_pic);
+  this.load.image("water_pic", water_pic);
+  this.load.image("cursor_pic", cursor_pic);
+  this.load.image("baby_duck_pic", baby_duck_pic);
+ 
 }
 
 function create() {
 
-  this.add.image(400, 300, "water");
+  console.log("from game.js");
+  //water background 
+  this.add.image(400, 300, "water_pic"); 
+  mama =this.physics.add.sprite(400,300,"mama_duck_pic").setScale(1.5).setOrigin(0.5,0.5);
+  mama.body.allowRotation = false;
+ 
+
+  curs = this.add.image(0,0,"cursor_pic").setOrigin(0.5,0.5);
+  var speed= 5;
+
+  baby=this.physics.add.sprite(200,200,"baby_duck_pic");
+  
+
+  /*
+  mama and baby collision function
+  this.hits= function(mama){
+    if(mama.y=== baby.y && mama.x===baby.x){
+      Debug.log("collision detected");
+      return true;
+    }
+  }
+  */
+//move to pointer
+this.input.on('pointermove',function(pointer){
+  curs.setVisible(true).setPosition(pointer.x,pointer.y);
+  this.physics.moveToObject(mama,curs,speed,240);
+},this);
+
 }
 
-function update() {}
+function update() {
+baby.angle+=1;
 
-const config = {
+}
+
+
+var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  parent:'phaser-example',
+  physics:{
+    default: 'arcade',
+    arcade:{ debug: true }
+  },
   scene: {
     preload: preload,
     create: create,
     update: update
+    
   }
 };
 
-export const game = new Phaser.Game(config);
+export var game = new Phaser.Game(config);
